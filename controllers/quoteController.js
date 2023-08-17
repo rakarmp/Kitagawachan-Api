5ef5ccb3676205159db1ed32b30971088e2f5299
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-08-17 08:39:11 +0700

    Fix no longer accepts a callback use async await

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -35,87 +35,101 @@ exports.createQuote = (req, res) => {
 };
 
 // Mendapatkan quote acak berdasarkan judul anime
-exports.getRandomQuoteByAnimeTitle = (req, res) => {
-  const { animeTitle } = req.params;
+exports.getRandomQuoteByAnimeTitle = async (req, res) => {
+  try {
+    const { animeTitle } = req.params;
 
-  Quote.countDocuments({ anime: animeTitle }).exec((err, count) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-
+    const count = await Quote.countDocuments({ anime: animeTitle });
     const random = Math.floor(Math.random() * count);
 
-    Quote.findOne({ anime: animeTitle })
-      .skip(random)
-      .then((quote) => {
-        if (!quote) {
-          return res.status(404).json({ message: "Quote tidak ditemukan" });
-        }
-        res.json(quote);
-      })
-      .catch((err) => res.status(500).json({ error: err.message }));
-  });
+    const quote = await Quote.findOne({ anime: animeTitle }).skip(random);
+
+    if (!quote) {
+      return res.status(404).json({ message: "Quote tidak ditemukan" });
+    }
+
+    res.json(quote);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Mendapatkan quote acak berdasarkan karakter anime
-exports.getRandomQuoteByAnimeCharacter = (req, res) => {
-  const { animeCharacter } = req.params;
+exports.getRandomQuoteByAnimeCharacter = async (req, res) => {
+  try {
+    const { animeCharacter } = req.params;
 
-  Quote.countDocuments({ character: animeCharacter }).exec((err, count) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-
+    const count = await Quote.countDocuments({ character: animeCharacter });
     const random = Math.floor(Math.random() * count);
 
-    Quote.findOne({ character: animeCharacter })
-      .skip(random)
-      .then((quote) => {
-        if (!quote) {
-          return res.status(404).json({ message: "Quote tidak ditemukan" });
-        }
-        res.json(quote);
-      })
-      .catch((err) => res.status(500).json({ error: err.message }));
-  });
+    const quote = await Quote.findOne({ character: animeCharacter }).skip(
+      random
+    );
+
+    if (!quote) {
+      return res.status(404).json({ message: "Quote tidak ditemukan" });
+    }
+
+    res.json(quote);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Mendapatkan 5 quote acak
-exports.getFiveRandomQuotes = (req, res) => {
-  Quote.aggregate([{ $sample: { size: 5 } }])
-    .then((quotes) => res.json(quotes))
-    .catch((err) => res.status(500).json({ error: err.message }));
+exports.getFiveRandomQuotes = async (req, res) => {
+  try {
+    const quotes = await Quote.aggregate([{ $sample: { size: 5 } }]);
+
+    res.json(quotes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Mendapatkan 5 quote acak berdasarkan judul anime
-exports.getFiveRandomQuotesByAnimeTitle = (req, res) => {
-  const { animeTitle } = req.params;
+exports.getFiveRandomQuotesByAnimeTitle = async (req, res) => {
+  try {
+    const { animeTitle } = req.params;
 
-  Quote.aggregate([{ $match: { anime: animeTitle } }, { $sample: { size: 5 } }])
-    .then((quotes) => res.json(quotes))
-    .catch((err) => res.status(500).json({ error: err.message }));
+    const quotes = await Quote.aggregate([
+      { $match: { anime: animeTitle } },
+      { $sample: { size: 5 } },
+    ]);
+
+    res.json(quotes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Mendapatkan 5 quote berdasarkan karakter anime
-exports.getFiveQuotesByAnimeCharacter = (req, res) => {
-  const { animeCharacter } = req.params;
+exports.getFiveQuotesByAnimeCharacter = async (req, res) => {
+  try {
+    const { animeCharacter } = req.params;
 
-  Quote.find({ character: animeCharacter })
-    .limit(5)
-    .then((quotes) => res.json(quotes))
-    .catch((err) => res.status(500).json({ error: err.message }));
+    const quotes = await Quote.find({ character: animeCharacter }).limit(5);
+
+    res.json(quotes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Mendapatkan quote dengan pagination
-exports.getQuotesWithPagination = (req, res) => {
-  const { page, limit } = req.query;
+exports.getQuotesWithPagination = async (req, res) => {
+  try {
+    const { page, limit } = req.query;
 
-  const options = {
-    page: parseInt(page, 10) || 1,
-    limit: parseInt(limit, 10) || 10,
-  };
+    const options = {
+      page: parseInt(page, 10) || 1,
+      limit: parseInt(limit, 10) || 10,
+    };
 
-  Quote.paginate({}, options)
-    .then((result) => res.json(result))
-    .catch((err) => res.status(500).json({ error: err.message }));
+    const result = await Quote.paginate({}, options);
+
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
